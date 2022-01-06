f03b8142cc82bf4d5e0f52366b57c6e0c4932f0d
RehamMuhammad <Reham.Muhammad.helmy@gmail.com>
2022-01-06 17:41:36 +0200

    Style Header Tabs Component

--- src/components/headerTabs/index.js
+++ src/components/headerTabs/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { View } from 'react-native'
 import { HeaderButton } from '../headerButtons'
+import { Styles } from './style'
 
  const HeaderTabs = () => {
     return (
-        <View>
+        <View style={Styles.headerTabsView}>
           <HeaderButton text="Delievery" />
           <HeaderButton text="Pickup"/>
         </View>
     )
 }
-
 
 export {HeaderTabs}
---
+++ src/components/headerTabs/style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+
+export const Styles = StyleSheet.create({
+    headerTabsView:{
+        flexDirection:"row",
+        alignSelf:"center"
+      
+    }
+
+})
